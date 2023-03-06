9488b6141ebfca6a2003042177f987e29e2de4fa
JakobKurtz <jkurtz@student.neumont.edu>
2023-03-06 14:13:49 -0700

    push

---
+++ src/main/java/com/example/swquiz/questionA.java
@@ -0,0 +1,14 @@
+package com.example.swquiz;
+
+import javafx.scene.input.MouseEvent;
+
+public class questionA {
+
+    public void AnsA1(MouseEvent mouseEvent) {
+
+    }
+
+    public void AnsA1(MouseEvent mouseEvent) {
+
+    }
+}
